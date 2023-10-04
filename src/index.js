17fbef8f7056e227cff18ffadccfb57ca7f66738
riadZk <riadzakaria48@gmail.com>
2023-10-04 21:06:31 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter} from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import './index.css';
-import App from './App';
+import App from './Nouveau dossier/App2';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store'
+// import {Provider} from 'react-redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
   <BrowserRouter>
-  <App /> 
-  </BrowserRouter>
-  </React.StrictMode>
+  {/* <Provider store={store}> */}
+    <App />
+    {/* </Provider> */}
+  </BrowserRouter>,
+  document.getElementById('root')
 );
+
 
 reportWebVitals();
